8a91cdcfde892842366e85a03c138e63dbeaa396
bs <soaqa57@gmail.com>
2023-11-01 19:48:55 +0300

    variables_and_types.py

---
+++ code/variables_and_types.py
@@ -0,0 +1,16 @@
+variable01 = 12
+variable02 = "Hello"
+variable03 = 1.5
+variable04 = []
+variable05 = {
+    variable01: variable02,
+    "key": variable03,
+    "key2": 10
+}
+variable06 = (variable01, variable02, variable03)
+# set
+variable07 = {1, 2, 3}
+variable08 = None
+
+print(type(variable07))
+print(variable07)
