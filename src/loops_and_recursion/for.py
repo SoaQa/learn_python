44897bd0e1e25f136f773132efeb4bcef7a76d03
soaqa <soaqa57@gmail.com>
2024-01-14 17:23:51 +0300

    for

---
+++ src/loops_and_recursion/for.py
@@ -0,0 +1,16 @@
+my_list: list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+for i in my_list:
+    print(i**2)
+
+# iterable types: list, dict, tuple, and set
+
+s = "Hello"
+for i in s:
+    print(i)
+
+
+my_int: int = 11
+for i in my_int:
+    print(i)
