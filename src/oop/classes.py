5f6327e07e331089b0223472598b447d38055a7b
soaqa <soaqa57@gmail.com>
2024-02-13 20:07:34 +0300

    first part

--- src/oop/classes.py
+++ src/oop/classes.py
@@ -1,9 +1,39 @@
 class TextReader(object):
-    file_name = "text"
-
     def __init__(self, file_name):
         self.file_name = file_name
 
+    def read_text(self,):
+        with open(self.file_name) as f:
+            print(f.read())
+
+
+# readers = {
+#     "text": TextReader("text"),
+#     "text2": TextReader("text2"),
+# }
+#
+# readers["text"].read_text()
+# readers["text2"].read_text()
+
+
+class ClassTextReader(object):
+    file_name = "text2"
+
     @classmethod
-    def read_text(cls, file_name):
-        print(cls(file_name))
+    def read_text(cls,):
+        with open(cls.file_name) as f:
+            print(f.read())
+
+#
+# readers = {
+#     "text": ClassTextReader(),
+#     "text2": ClassTextReader(),
+# }
+#
+# readers["text"].read_text()
+# readers["text2"].read_text()
+#
+# ClassTextReader.file_name = "text1"
+#
+# readers["text"].read_text()
+# readers["text2"].read_text()
--- src/oop/main.py
+++ src/oop/main.py
@@ -2,5 +2,6 @@ from oop import classes
 
 
 if __name__ == '__main__':
-    classes.TextReader.read_text("text")
+    classes.TextReader("text").read_text()
+    classes.ClassTextReader().read_text()
 
