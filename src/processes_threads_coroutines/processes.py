4bdec2ab4d03bc51f74163e7d3d5acde5788dbc8
soaqa <soaqa57@gmail.com>
2024-05-25 16:04:55 +0300

    create process

--- src/processes_threads_coroutines/processes.py
+++ src/processes_threads_coroutines/processes.py
@@ -1,9 +1,16 @@
+import random
 from time import sleep
+import multiprocessing
+import os
 
 
 def main():
-    sleep(100)
+    for i in range(10):
+        print(os.getpid())
+        sleep(random.randint(1, 5))
 
 
 if __name__ == '__main__':
+    proc = multiprocessing.Process(target=main)
+    proc.start()
     main()
