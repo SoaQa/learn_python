086aa6380bc2dd360eea5b666675dbb4b1458491
soaqa <soaqa57@gmail.com>
2024-05-25 16:32:33 +0300

    create processes

--- src/processes_threads_coroutines/processes.py
+++ src/processes_threads_coroutines/processes.py
@@ -1,16 +1,28 @@
-import random
-from time import sleep
 import multiprocessing
-import os
+import time
 
 
-def main():
-    for i in range(10):
-        print(os.getpid())
-        sleep(random.randint(1, 5))
+def main(from_int, to_int, rl):
+    result = 0
+    for i in range(from_int, to_int):
+        result += i ** 2
+
+    rl.append(result)
 
 
 if __name__ == '__main__':
-    proc = multiprocessing.Process(target=main)
-    proc.start()
-    main()
+    start = time.time()
+    manager = multiprocessing.Manager()
+    result_list = manager.list()
+
+    proc1 = multiprocessing.Process(target=main, args=(1, 5_000_000, result_list))
+    proc2 = multiprocessing.Process(target=main, args=(5_000_001, 10_000_000, result_list))
+
+    proc1.start()
+    proc2.start()
+
+    proc1.join()
+    proc2.join()
+
+    print(sum(result_list))
+    print(time.time() - start)
